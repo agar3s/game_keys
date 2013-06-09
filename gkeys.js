fd8dac46210fb0c8c534ee3e7db77d5ad9b85253
Gio <ekeisco@gmail.com>
2013-06-09 16:22:37 -0500

    map factoring

--- gkeys.js
+++ gkeys.js
@@ -1,53 +1,58 @@
 var gKeys = function(){
   var keys = {
-              UP: {down:false, up:false, pressed:false},
-              DOWN: {down:false, up:false, pressed:false},
-              LEFT: {down:false, up:false, pressed:false},
-              RIGHT: {down:false, up:false, pressed:false}
+              UP: {d:false, u:false, p:0},
+              DOWN: {d:false, u:false, p:0},
+              LEFT: {d:false, u:false, p:0},
+              RIGHT: {d:false, u:false, p:0}
              };
-
-  var kdCallback, kuCallback;
+  var mapKeys = {
+                37: 'LEFT',
+                38: 'UP',
+                39: 'RIGHT',
+                40: 'DOWN'
+              };
+  var kdCallback, kuCallback = function(keys){};
   var canvas;
 
-  var mapping = function(key, state){
-    keys.UP[state] = key == 38;
-    keys.DOWN[state] = key == 40;
-    keys.LEFT[state] = key == 37;
-    keys.RIGHT[state] = key == 39;
+  var mapping = function(key, down){
+    if(key in mapKeys){
+      keys[mapKeys[key]].d = down;
+      keys[mapKeys[key]].u = !down;
+    }
+    for(key in keys){
+      if (!keys.hasOwnProperty(key)) continue;
+      keys[key].p = keys[key].d ? keys[key].p+1 : 0;
+    }
   };
 
   var keyDown = function(e){
     var key = e.keyCode ? e.keyCode : e.which;
-    mapping(key, 'down');
+    mapping(key, true);
     kdCallback(keys);
     e.preventDefault();
   };
 
   var keyUp = function(e){
     var key = e.keyCode ? e.keyCode : e.which;
-    mapping(key, 'up');
+    mapping(key, false);
     kuCallback(keys);
     e.preventDefault();
   };
 
-  var init = function(){
+  (function(){
     canvas = document.getElementsByTagName('canvas')[0];
     canvas.setAttribute('tabIndex', 1);
+    canvas.addEventListener('keydown',  keyDown);
+    canvas.addEventListener('keyup',  keyUp);
     canvas.focus();
-  };
-
-  (function(){
-    init();
   })();
 
   return {
     'keyDown': function(callback){
       kdCallback = callback;
-      canvas.addEventListener('keydown',  keyDown);
     },
     'keyUp': function(callback){
       kuCallback = callback;
-      canvas.addEventListener('keyup',  keyUp);
     }
   };
 
