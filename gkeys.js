22666e14f9a712b41396711b48fdaed85fbd7785
Gio <ekeisco@gmail.com>
2013-06-09 12:32:33 -0500

    mapping and initialization

--- gkeys.js
+++ gkeys.js
@@ -1,37 +1,53 @@
 var gKeys = function(){
   var keys = {
-              'UP': true,
-              'DOWN': true,
-              'LEFT': true,
-              'RIGHT':true
+              UP: {down:false, up:false, pressed:false},
+              DOWN: {down:false, up:false, pressed:false},
+              LEFT: {down:false, up:false, pressed:false},
+              RIGHT: {down:false, up:false, pressed:false}
              };
 
   var kdCallback, kuCallback;
+  var canvas;
+
+  var mapping = function(key, state){
+    keys.UP[state] = key == 38;
+    keys.DOWN[state] = key == 40;
+    keys.LEFT[state] = key == 37;
+    keys.RIGHT[state] = key == 39;
+  };
 
   var keyDown = function(e){
     var key = e.keyCode ? e.keyCode : e.which;
-    keys['UP'] = key == 38;
-    keys['DOWN'] = key == 40;
-    keys['LEFT'] = key == 37;
-    keys['RIGHT'] = key == 39;
-    
+    mapping(key, 'down');
     kdCallback(keys);
-  }
+    e.preventDefault();
+  };
 
   var keyUp = function(e){
     var key = e.keyCode ? e.keyCode : e.which;
+    mapping(key, 'up');
     kuCallback(keys);
-  }
+    e.preventDefault();
+  };
 
+  var init = function(){
+    canvas = document.getElementsByTagName('canvas')[0];
+    canvas.setAttribute('tabIndex', 1);
+    canvas.focus();
+  };
+
+  (function(){
+    init();
+  })();
 
   return {
     'keyDown': function(callback){
       kdCallback = callback;
-      document.addEventListener('keydown',  keyDown);
+      canvas.addEventListener('keydown',  keyDown);
     },
     'keyUp': function(callback){
       kuCallback = callback;
-      document.addEventListener('keyup',  keyUp);
+      canvas.addEventListener('keyup',  keyUp);
     }
   };
 
