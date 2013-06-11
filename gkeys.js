22846c7cd5c7ba4531d9e9ab06b9105d51bbb061
Agares <ekeisco@gmail.com>
2013-06-11 13:23:17 -0500

    solves the keypress problem

--- gkeys.js
+++ gkeys.js
@@ -1,59 +1,83 @@
-var gKeys = function(){
-  var keys = {
-              UP: {d:false, u:false, p:0},
-              DOWN: {d:false, u:false, p:0},
-              LEFT: {d:false, u:false, p:0},
-              RIGHT: {d:false, u:false, p:0}
-             };
-  var mapKeys = {
-                37: 'LEFT',
-                38: 'UP',
-                39: 'RIGHT',
-                40: 'DOWN'
-              };
-  var kdCallback, kuCallback = function(keys){};
-  var canvas;
+(function(window, document, undefined){
 
-  var mapping = function(key, down){
-    if(key in mapKeys){
-      keys[mapKeys[key]].d = down;
-      keys[mapKeys[key]].u = !down;
-    }
-    for(key in keys){
-      if (!keys.hasOwnProperty(key)) continue;
-      keys[key].p = keys[key].d ? keys[key].p+1 : 0;
-    }
-  };
+  var gKeys = function(){
+    var keys = {
+                UP: {d:false, u:false, p:0},
+                DOWN: {d:false, u:false, p:0},
+                LEFT: {d:false, u:false, p:0},
+                RIGHT: {d:false, u:false, p:0}
+               },
+        mapKeys = {
+                  37: 'LEFT',
+                  38: 'UP',
+                  39: 'RIGHT',
+                  40: 'DOWN'
+                },
+        canvas,
+        kdCallback = function( keys ){},
+        kuCallback = function( keys ){};
 
-  var keyDown = function(e){
-    var key = e.keyCode ? e.keyCode : e.which;
-    mapping(key, true);
-    kdCallback(keys);
-    e.preventDefault();
-  };
 
-  var keyUp = function(e){
-    var key = e.keyCode ? e.keyCode : e.which;
-    mapping(key, false);
-    kuCallback(keys);
-    e.preventDefault();
-  };
+    var mapping = function( key, down ){
+      if(key in mapKeys){
+        keys[mapKeys[key]].d = down;
+        keys[mapKeys[key]].u = !down;
+      }
+    };
 
-  (function(){
-    canvas = document.getElementsByTagName('canvas')[0];
-    canvas.setAttribute('tabIndex', 1);
-    canvas.addEventListener('keydown',  keyDown);
-    canvas.addEventListener('keyup',  keyUp);
-    canvas.focus();
-  })();
+    var keyDowns = function( e ){
+      var key = e.keyCode ? e.keyCode : e.which;
+      mapping(key, true);
+      e.preventDefault();
+    };
 
-  return {
-    'keyDown': function(callback){
-      kdCallback = callback;
-    },
-    'keyUp': function(callback){
-      kuCallback = callback;
-    }
-  };
+    var keyUps = function( e ){
+      var key = e.keyCode ? e.keyCode : e.which;
+      mapping(key, false);
+      e.preventDefault();
+    };
 
-}();
+    var scanKeys = function(){
+      var callUp = false,
+          callDown = false;
+
+      for(key in keys){
+        if (!keys.hasOwnProperty(key)) continue;
+        keys[key].p = keys[key].d ? keys[key].p+1 : 0;
+        callDown = callDown || keys[key].d;
+        callUp = callUp || keys[key].u;
+      }
+      if(callDown){
+        kdCallback(keys);
+      }
+      if(callUp){
+        kuCallback(keys);
+      }
+    };
+
+    (function init(){
+      canvas = document.getElementsByTagName('canvas')[0];
+      canvas.setAttribute('tabIndex', 1);
+      canvas.addEventListener('keydown',  keyDowns);
+      canvas.addEventListener('keyup',  keyUps);
+      canvas.focus();
+    })();
+    
+    (function scanning(){
+      scanKeys();  
+      window.setTimeout(scanning, 1000 / 60);
+    })();
+
+    return {
+      'keyDown': function( callback ){
+        kdCallback = callback;
+      },
+      'keyUp': function( callback ){
+        kuCallback = callback;
+      }
+    };
+
+  }();
+  window.gKeys = gKeys;
+
+})(window, document);
